a52ae20932992b65b10cf7db4224212bc8b82544
dajojo <dajojo1403@gmail.com>
2023-02-17 19:36:18 +0100

    fixed artefact issue

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -29,7 +29,10 @@ class AnimationCard extends StatelessWidget {
       animation: animation!,
       builder: (BuildContext context, Widget? child) {
         var transform = Matrix4.identity();
-        transform.setEntry(3, 2, 0.001);
+        if (animation!.status == AnimationStatus.forward ||
+            animation!.status == AnimationStatus.reverse) {
+          transform.setEntry(3, 2, 0.001);
+        }
         if (direction == FlipDirection.VERTICAL) {
           transform.rotateX(animation!.value);
         } else {
